d6dda12b5469bb86b4aaf8b56e1105214d1caf55
kristyknapp <kristyknapp@10.18.148.160>
2014-05-21 09:52:37 -0600

    Team Assignment Week 4

--- chooseYourOwnAdventure/test/byui/cit260/chooseYourOwnAdventure/control/PointsControlTest.java
+++ chooseYourOwnAdventure/test/byui/cit260/chooseYourOwnAdventure/control/PointsControlTest.java
@@ -34,14 +34,98 @@ public class PointsControlTest {
     @Test
     public void testCalcTotalPoints() {
         System.out.println("calcTotalPoints");
-        double sleepPoints = 0.0;
-        double healthPoints = 0.0;
+        
+        /*Test Case #1*/
+        System.out.println("Test case #1");
+        
+        //inpute values for case 1
+        double sleepPoints = 37.0;
+        double healthPoints = 53.0;
         PointsControl instance = new PointsControl();
-        double expResult = 0.0;
+        
+        double expResult = 90.0; //expected output return value
+        
         double result = instance.calcTotalPoints(sleepPoints, healthPoints);
         assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+                /*Test Case #2*/
+        System.out.println("Test case #2");
+        
+        //inpute values for case 2
+        sleepPoints = -1.0;
+        healthPoints = 63.0;
+        instance = new PointsControl();
+        
+        expResult = -1.0; //expected output return value
+        
+        result = instance.calcTotalPoints(sleepPoints, healthPoints);
+        assertEquals(expResult, result, 0.0);
+        
+                        /*Test Case #3*/
+        System.out.println("Test case #3");
+        
+        //inpute values for case 3
+        sleepPoints = 40.0;
+        healthPoints = -1.0;
+        instance = new PointsControl();
+        
+        expResult = -1.0; //expected output return value
+        
+        result = instance.calcTotalPoints(sleepPoints, healthPoints);
+        assertEquals(expResult, result, 0.0);
+        
+                        /*Test Case #4*/
+        System.out.println("Test case #4");
+        
+        //inpute values for case 4
+        sleepPoints = 41.0;
+        healthPoints = 30.0;
+        instance = new PointsControl();
+        
+        expResult = -1.0; //expected output return value
+        
+        result = instance.calcTotalPoints(sleepPoints, healthPoints);
+        assertEquals(expResult, result, 0.0);
+        
+                        /*Test Case #5*/
+        System.out.println("Test case #5");
+        
+        //inpute values for case 5
+        sleepPoints = 40.0;
+        healthPoints = 60.0;
+        instance = new PointsControl();
+        
+        expResult = 100.0; //expected output return value
+        
+        result = instance.calcTotalPoints(sleepPoints, healthPoints);
+        assertEquals(expResult, result, 0.0);
+        
+                        /*Test Case #6*/
+        System.out.println("Test case #6");
+        
+        //inpute values for case 6
+        sleepPoints = 37.0;
+        healthPoints = 60.0;
+        instance = new PointsControl();
+        
+        expResult = 97.0; //expected output return value
+        
+        result = instance.calcTotalPoints(sleepPoints, healthPoints);
+        assertEquals(expResult, result, 0.0);
+        
+                        /*Test Case #7*/
+        System.out.println("Test case #7");
+        
+        //inpute values for case 7
+        sleepPoints = 40.0;
+        healthPoints = 53.0;
+        instance = new PointsControl();
+        
+        expResult = 93.0; //expected output return value
+        
+        result = instance.calcTotalPoints(sleepPoints, healthPoints);
+        assertEquals(expResult, result, 0.0);
+       
     }
     
 }
