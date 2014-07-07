6855d929784ed515a8431a988be4c09cab93210f
tannercw <twooly@cableone.net>
2014-07-07 09:12:08 -0600

    Commit 2nd View Layer

--- chooseYourOwnAdventure/src/byui/cit260/chooseYourOwnAdventure/view/HelpGoalsView.java
+++ chooseYourOwnAdventure/src/byui/cit260/chooseYourOwnAdventure/view/HelpGoalsView.java
@@ -8,12 +8,20 @@ package byui.cit260.chooseYourOwnAdventure.view;
 
 /**
  *
- * @author kristyknapp
+ * @author Tanner
  */
 public class HelpGoalsView {
 
-    public void displayMenu() {
-        System.out.println("*** HelpGoalsView displayMenu stub function called ***");
+    public void displayHelpGoalsView() {
+        System.out.println("The goal of the game, simply is to get off the island <n>"
+                + "by any means possible. You choose an area of the island to explore,<n>"
+                + "find resources to escape, all the while keeping yourself healthy enough<n>"
+                + "to survive. Every action you make costs you, so you must constantly keep <n>"
+                + "healthy and any survivors you might find and take care of. If you move, <n>"
+                + "you lose health, if you attempt to escape, you lose health, if you <n>"
+                + "hardly breath you lose health, so weigh your options carefully.");
+        System.out.println("The outcome of your rescue is entirely randomly generated.<n>"
+                + "You might be successful on your first try, or you might not be until<n>"
+                + "your 12th try. Always keep in mind that those attempts cost your health as well.");
     }
-    
 }
