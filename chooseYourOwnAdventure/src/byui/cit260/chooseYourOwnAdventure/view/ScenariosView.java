f56addce8d632d88bc10bc0cf72bb3a660c4b4fc
tannercw <twooly@cableone.net>
2014-07-07 09:12:43 -0600

    Commit 2nd View Layer

--- chooseYourOwnAdventure/src/byui/cit260/chooseYourOwnAdventure/view/ScenariosView.java
+++ chooseYourOwnAdventure/src/byui/cit260/chooseYourOwnAdventure/view/ScenariosView.java
@@ -8,12 +8,24 @@ package byui.cit260.chooseYourOwnAdventure.view;
 
 /**
  *
- * @author kristyknapp
+ * @author Tanner
  */
 public class ScenariosView {
 
-    public void displayMenu() {
-        System.out.println("*** ScenariosView displayMenu stub function called ***");
+    public void displaySenariosView() {
+        System.out.println
+        ("There are many different senarios that you will come across<n>"
+        + "within the game. First being choosing your section of the<n>"
+        + "island you will interact with, the beach, or the cliffs.<n>"
+        + "The beach offers better rescue options while the cliffs<n>"
+        + "offers better resources for survival. It's your choice.<n>");
+        
+        System.out.println 
+        ("The next choices you will come across are up to your previous choices.<n>"
+        + "You must start to explore the part of the island you are on and <n>"
+        + "collect resources. Logs, rocks and vines are your means of rescue,<n>"
+        + "while food and water are impartiveto your long term survival.<n>"
+        + "Moving locations and attempting rescue will cost your health.<n>"
+        + "You must balance working with your eating, drinking and sleeping.<n>");
     }
-    
 }
