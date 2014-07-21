f7e2a2bd1d24205beb477a02469db16c66d2c537
tannercw <twooly@cableone.net>
2014-07-21 14:58:00 -0600

    Commit RescueOptions Overhaul 2

---
+++ chooseYourOwnAdventure/src/byui/cit260/chooseYourOwnAdventure/view/RaftView.java
@@ -0,0 +1,58 @@
+*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.chooseYourOwnAdventure.view;
+
+import byui.cit260.chooseYourOwnAdventure.control.ProgramControl;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tanner
+ *
+**/
+
+public abstract class RaftView extends MenuView{
+
+    public RaftView() {
+        super("\n-----------------------------------------------------------"
+                + "\n|Build a Raft                                              "
+                + "\n-----------------------------------------------------------"
+                + "\n In order to get off the island, you must collect enough   "
+                + "\n resources to build a raft. 1/2x6 logs are needed to create  "
+                + "\n a large enough raft to float you and other survivors.     "
+                + "\n One person requires at least a 3x6 raft.                  "
+                + "\n Two people require at least a 3x12 raft.                  "
+                + "\n Three people require ate least a 3x18 raft.               "
+                + "\n-----------------------------------------------------------"
+                + "\n Please enter the ammount of logs you have to build your raft. "
+                + "\n Remember, you must have at least 6 logs if it is only you,    "
+                + "\n 12 logs if it's you an another survivor,                      "
+                + "\n and 18 logs if you have two others with you.                  "
+                + "\n\n-------------------------------------------------------------");
+    }
+   public String raftChoice (String value) {
+            value = value.toUpperCase();
+                switch (String value) {
+                    case "Y"://TAKE AWAY TEN HEALTH POINTS
+                    int= Math.random(1);
+                        if (Math.random=0); //SUCCESS
+                            System.out.println ("Congratulations, you have escaped the island."
+                            +"Do you want to play again? [Y] or [N]?");
+                                public String raftChoiceTwo (String value) {
+                                    value = value.toUpperCase();
+                                    switch (String value) {
+                                        case "Y": 
+                                            startProgramView();
+                                        case "N":
+                                            dispose();
+                        if (Math.random="1"); //FAILURE
+                            System.out.println ("Failure. Please Try again when ready.");
+                                MainMenuView(); //return to the previous menu
+                    case "N":
+                        MainMenuView(); //Return to the previous menu
+        }
+    }
+    
--- chooseYourOwnAdventure/src/byui/cit260/chooseYourOwnAdventure/view/RescueOptionsView.java
+++ chooseYourOwnAdventure/src/byui/cit260/chooseYourOwnAdventure/view/RescueOptionsView.java
@@ -24,28 +24,66 @@ public class RescueOptionsView extends MenuView {
             +"\nF- Build Signal Fire                                         "
             +"\nP - Call out to a Plane                                      "
             +"\nR - Build Raft                                               "
-            +"\nM - Write Message in Sand                                    "
+            +"\nM - Write Message in Sand/Vines                                    "
             +"\nE - Exit                                                     "
             +"\n-------------------------------------------------------------");
  
     public boolean doAction(value); String = null choice;{
         switch (value); {
-            case "F":  //create a new game
-                FireView fireView =
-                fireView.displayFireView();
+            case "F":  //build fire
+                FireView fireView = new FireView();
+                fireView.getFireView();
                 break;
-            case "P": // display the help menu
-                CallPlaneView planeView = new CallPlaneView();
-                planeView.displayView();
+            case "P": // call out to a plane
+                PlaneView planeView = new PlaneView();
+                planeView.getPlaneView();
                 break;
-            case "S": //save the current game to disk
-                ProgramControl.saveGame(ChooseYourOwnAdventure.getCurrentGame());
+            case "R": //build raft
+                RaftView raftView =
+                raftView.displayRaftView();
+            case "M": //Write Message
+                MessageView messageView();
+                messageView.getMessageView;
                 break;
             case "E": // Exit the program
                 this.dispose;
             default:
-                System.out.println("/n*** Invalid selection *** Try again");
+                System.out.println("*** Invalid selection. Try again ***");
                 break;
         } 
     }
 
+    public class FireView {
+    value = value.toUpperCase();
+        switch (String value); {
+            case "F": 
+                this.buildFire();//Build a fire
+                System.out.println     
+                ("Create signal fire to try and be seen"
+                + "by possibly nearby ships and planes."
+                + "Creating a Signal fire will take away "
+                + "10 health points and requires you to "
+                + "have at least one log."
+                + "Do you want to build a fire? [Y] or [N]");
+        
+        public String fireChoice(String value); {
+         value = value.toUpperCase();}
+                switch (String value); {
+                    case "Y"://TAKE AWAY TEN HEALTH POINTS
+                    int= Math.random(1);
+                        if (Math.random=0); //SUCCESS
+                            System.out.println ("Congratulations, you have escaped the island."
+                            +"Do you want to play again? [Y] or [N]?");
+                                
+                                public String playAgain(String value){
+                                    value = value.toUpperCase();
+                                    switch (String value) {
+                                        case "Y": 
+                                            this.startProgramView();
+                                        case "N":
+                                            this.dispose();
+                        if (Math.random="1"); //FAILURE
+                            System.out.println ("Failure. Please Try again when ready.");
+                                this.MainMenuView(); //return to the previous menu
+                    case "N":
+                        this.MainMenuView; //Return to the previous menu
